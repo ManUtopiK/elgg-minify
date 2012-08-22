ee9675609116965fa813f8eb85c8d54caeaf8037
ManUtopiK <emmanuel.salomon@free.fr>
2012-08-22 20:12:20 +0200

    parse only css/elgg and js/elgg

--- start.php
+++ start.php
@@ -2,20 +2,19 @@
 
 function minify_init() {
 	//make sure this runs after everyone else is done
-	elgg_register_plugin_hook_handler('view', 'all', 'minify_views', 1000);
+	elgg_register_plugin_hook_handler('view', 'js/elgg', 'minify_views_js', 1000);
+	elgg_register_plugin_hook_handler('view', 'css/elgg', 'minify_views_css', 1000);
 }
 
-function minify_views($hook, $type, $content, $params) {
-	$view = $params['view'];
+function minify_views_js($hook, $type, $content, $params) {
+	if (include_once dirname(__FILE__) . '/vendors/min/lib/JSMin.php') {
+		return JSMin::minify($content);
+	}
+}
 
-	if (preg_match("/^js\//", $view)) {
-		if (include_once dirname(__FILE__) . '/vendors/min/lib/JSMin.php') {
-			return JSMin::minify($content);
-		}
-	} elseif (preg_match("/^css\//", $view)) {
-		if (include_once dirname(__FILE__) . '/vendors/min/lib/CSS.php') {
-			return Minify_CSS::minify($content);
-		}
+function minify_views_css($hook, $type, $content, $params) {
+	if (include_once dirname(__FILE__) . '/vendors/min/lib/CSS.php') {
+		return Minify_CSS::minify($content);
 	}
 }
 
